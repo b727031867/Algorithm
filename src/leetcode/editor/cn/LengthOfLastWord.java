77b0b1185f5747e6d89679c9a408665c3dcd3019
gxf <727031867@qq.com>
2020-07-11 08:27:39 +0800

    算法题项目初始化

---
+++ src/leetcode/editor/cn/LengthOfLastWord.java
@@ -0,0 +1,33 @@
+//给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。 
+//
+// 如果不存在最后一个单词，请返回 0 。 
+//
+// 说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串。 
+//
+// 
+//
+// 示例: 
+//
+// 输入: "Hello World"
+//输出: 5
+// 
+// Related Topics 字符串 
+// 👍 212 👎 0
+
+//题目编号：58
+package leetcode.editor.cn;
+//Java：最后一个单词的长度
+public class P58LengthOfLastWord{
+    public static void main(String[] args) {
+        Solution solution = new P58LengthOfLastWord().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLastWord(String s) {
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
